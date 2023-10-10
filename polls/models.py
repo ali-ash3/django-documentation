2d4aaae41b2978055e5212db3eb27a4656acc4f5
Ali <alibinashraf1@gmail.com>
2023-10-10 13:09:20 +0500

    completed tutorial 1, 2, 3

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+    def __str__(self):
+        return self.question_text
+
+
+class choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.choice_text
