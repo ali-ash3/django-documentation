396271c7cd3b9bd8b66d262917253cd2496c47ae
Ali <alibinashraf1@gmail.com>
2023-10-12 09:43:46 +0500

    tut 4 half

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from polls.models import Question, choice
+from polls.models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "question_text", "pub_date",)
+admin.site.register(Question, QuestionAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "question", "choice_text","votes")
+admin.site.register(Choice, ChoiceAdmin)
